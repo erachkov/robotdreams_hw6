6d9998406201fa4ccdffd70466d1aace3c7a318b
irachkov <irachkov>
2021-06-02 00:27:53 +0300

    updated pyspark

--- pyspark.py
+++ pyspark.py
@@ -39,37 +39,37 @@ category_df = spark.read.jdbc(pg_url, 'public.category', properties=pg_propertie
 film_category_df = spark.read.jdbc(pg_url, 'public.film_category', properties=pg_properties)
 
 
-# In[50]:
+# In[7]:
 
 
 actor_df = spark.read.jdbc(pg_url, 'public.actor', properties=pg_properties)
 
 
-# In[51]:
+# In[8]:
 
 
 film_actor_df = spark.read.jdbc(pg_url, 'public.film_actor', properties=pg_properties)
 
 
-# In[52]:
+# In[9]:
 
 
 inventory_df = spark.read.jdbc(pg_url, 'public.inventory', properties=pg_properties)
 
 
-# In[53]:
+# In[10]:
 
 
 rental_df = spark.read.jdbc(pg_url, 'public.rental', properties=pg_properties)
 
 
-# In[ ]:
+# In[11]:
 
 
+payment_df = spark.read.jdbc(pg_url, 'public.payment', properties=pg_properties)
 
 
-
-# In[7]:
+# In[12]:
 
 
 # 1    вывести количество фильмов в каждой категории, отсортировать по убыванию.
@@ -87,31 +87,31 @@ rental_df = spark.read.jdbc(pg_url, 'public.rental', properties=pg_properties)
 
 
 
-# In[11]:
+# In[13]:
 
 
 stg_film_category_df = film_category_df.join(film_df, film_category_df.film_id == film_df.film_id, 'inner'  ).select(film_category_df.category_id)
 
 
-# In[12]:
+# In[14]:
 
 
 core_film_category_df = stg_film_category_df.join(category_df, stg_film_category_df.category_id == category_df.category_id, 'inner').select(category_df.name)
 
 
-# In[48]:
+# In[15]:
 
 
 final_df = core_film_category_df.groupBy('name').agg(F.count("name").alias("count_df")).orderBy(F.col("count_df").desc())
 
 
-# In[49]:
+# In[16]:
 
 
 final_df.show()
 
 
-# In[ ]:
+# In[17]:
 
 
 #-- 2   вывести 10 актеров, чьи фильмы большего всего арендовали, отсортировать по убыванию.
@@ -129,32 +129,144 @@ final_df.show()
 #    ) sub limit 10;
 
 
-# In[58]:
+# In[18]:
 
 
 stg_film_actor_df = film_actor_df.join(actor_df,actor_df.actor_id == film_actor_df.actor_id, 'inner' ).select(actor_df.first_name,actor_df.last_name, film_actor_df.film_id)
 
 
-# In[60]:
+# In[19]:
 
 
 stg_film_actor_df = stg_film_actor_df.join(film_df, film_df.film_id == stg_film_actor_df.film_id, 'inner').select(stg_film_actor_df.first_name,stg_film_actor_df.last_name, film_df.film_id)
 
 
-# In[62]:
+# In[20]:
 
 
 stg_inventory_df = inventory_df.join(stg_film_actor_df, inventory_df.film_id == stg_film_actor_df.film_id, 'inner').select(stg_film_actor_df.first_name,stg_film_actor_df.last_name, stg_film_actor_df.film_id,inventory_df.inventory_id)
 
 
-# In[65]:
+# In[21]:
 
 
 stg_rental_df = rental_df.join(stg_inventory_df,stg_inventory_df.inventory_id == rental_df.inventory_id, 'inner').select(stg_inventory_df.first_name,stg_inventory_df.last_name, stg_inventory_df.film_id,rental_df.inventory_id)
 
 
-# In[70]:
+# In[22]:
 
 
 stg_rental_df.groupBy('first_name','last_name').agg(F.count("*").alias("rent_quantity")).orderBy(F.col("rent_quantity").desc()).show(10)
+
+
+# In[23]:
+
+
+#-- 3    вывести категорию фильмов, на которую потратили больше всего денег.
+
+#select cat.name,
+#       sum(p.amount) film_amount
+#from category cat
+#join film_category fcat ON fcat.category_id = cat.category_id
+#join film f ON f.film_id = fcat.film_id
+#join inventory i ON i.film_id = f.film_id
+#join rental r on r.inventory_id = i.inventory_id
+#join payment p on p.rental_id = r.rental_id
+#group by cat.name
+#order by 2 desc;
+
+
+# In[24]:
+
+
+stg_film_category_df = film_category_df.join(film_df, film_category_df.film_id == film_df.film_id, 'inner'  ).select(film_category_df.category_id,film_category_df.film_id)
+
+
+# In[25]:
+
+
+core_film_category_df = stg_film_category_df.join(category_df, stg_film_category_df.category_id == category_df.category_id, 'inner').select(stg_film_category_df.film_id, category_df.name)
+
+
+# In[26]:
+
+
+stg_inventory_df = inventory_df.join(core_film_category_df, inventory_df.film_id == core_film_category_df.film_id, 'inner').select(core_film_category_df.name,core_film_category_df.film_id,inventory_df.inventory_id)
+
+
+# In[32]:
+
+
+stg_rental_df = rental_df.join(stg_inventory_df,stg_inventory_df.inventory_id == rental_df.inventory_id, 'inner').select(stg_inventory_df.name,stg_inventory_df.film_id,stg_inventory_df.inventory_id,rental_df.rental_id).show()
+
+
+# In[29]:
+
+
+payment_df.show()
+
+
+# In[33]:
+
+
+stg_payment_df = payment_df.join(stg_rental_df, payment_df.rental_id == stg_rental_df.rental_id, 'inner')
+#.select(stg_rental_df.name,stg_rental_df.film_id, stg_payment_df.amount)\
+
+
+# In[ ]:
+
+
+#--  4   вывести названия фильмов, которых нет в inventory. Написать запрос без использования оператора IN.
+
+#select f.title from
+#    film f
+#where not exists
+#    (select 1 from inventory i where i.film_id = f.film_id);
+
+
+# In[ ]:
+
+
+#-- 5    вывести топ 3 актеров, которые больше всего появлялись в фильмах в категории “Children”. Если у нескольких актеров одинаковое кол-во фильмов, вывести всех
+
+
+# In[ ]:
+
+
+#--  6  вывести города с количеством активных и неактивных клиентов (активный — customer.active = 1). Отсортировать по количеству неактивных клиентов по убыванию.
+
+#SELECT c.city,
+#       cy.country,
+#       cust.active,
+#       count(*)
+#FROM customer cust
+#         JOIN rental r ON r.customer_id = cust.customer_id
+#         JOIN inventory i ON r.inventory_id = i.inventory_id
+#         JOIN store s ON i.store_id = s.store_id
+#         JOIN address a ON s.address_id = a.address_id
+#         JOIN city c ON a.city_id = c.city_id
+#         JOIN country cy ON c.country_id = cy.country_id
+#GROUP BY cy.country, c.city, cust.active
+#ORDER BY cust.active, cy.country, c.city;
+
+
+# In[ ]:
+
+
+#-  7   вывести категорию фильмов, у которой самое большое кол-во часов суммарной аренды в городах (customer.address_id в этом city), и которые начинаются на букву “a”. То же самое сделать для городов в которых есть символ “-”. Написать все в одном запросе.#
+
+#select cat.name,
+#       c.city,
+#       sum(round(extract(epoch from (r.return_date) - (r.rental_date))/3600)) as sum_hours
+#from category cat
+#join film_category fcat ON fcat.category_id = cat.category_id
+#join film f ON f.film_id = fcat.film_id
+#join inventory i ON i.film_id = f.film_id
+#join rental r on r.inventory_id = i.inventory_id
+#join customer cust on r.customer_id = cust.customer_id
+#JOIN address a ON cust.address_id = a.address_id
+#JOIN city c ON a.city_id = c.city_id
+#where city like 'a%' or city like '%-%'
+#group by cat.name, c.city having sum(round(extract(epoch from (r.return_date) - (r.rental_date))/3600)) >0
+#order by 3 desc;
 
